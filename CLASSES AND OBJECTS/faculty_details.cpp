f8029a1f2e473bf632caebafc4a3ac5c6381fd16
Shivam Jaiswal <153131745+Shivam0400@users.noreply.github.com>
2025-03-25 12:19:59 +0530

    Update faculty_details.cpp

--- CLASSES AND OBJECTS/faculty_details.cpp
+++ CLASSES AND OBJECTS/faculty_details.cpp
@@ -1,8 +1,7 @@
-#include <iostream>
+#include <iostream> 
 #include <string>
 using namespace std;
-
-class Faculty
+class Faculty 
 {
     string id;
     string name;
@@ -11,22 +10,25 @@ class Faculty
     string address;
 
 public:
-    void assignInitialValues(string facultyID, string facultyName, string facultyPost, string facultyQualification, string facultyAddress) {
-        id = facultyID;
-        name = facultyName;
-        post = facultyPost;
-        qualification = facultyQualification;
-        address = facultyAddress;
-    }
-
-    void display() {
-        cout << "Faculty ID: " << id << endl;
-        cout << "Name: " << name << endl;
-        cout << "Post: " << post << endl;
-        cout << "Qualification: " << qualification << endl;
-        cout << "Address: " << address << endl << endl;
-    }
+    void assignInitialValues(string facultyID, string facultyName, string facultyPost, string facultyQualification, string facultyAddress);
+    void display();
 };
+
+void Faculty::assignInitialValues(string facultyID, string facultyName, string facultyPost, string facultyQualification, string facultyAddress) {
+    id = facultyID;
+    name = facultyName;
+    post = facultyPost;
+    qualification = facultyQualification;
+    address = facultyAddress;
+}
+void Faculty::display() {
+    cout << "\nFaculty Details:" << endl;
+    cout << "Faculty ID: " << id << endl;
+    cout << "Name: " << name << endl;
+    cout << "Post: " << post << endl;
+    cout << "Qualification: " << qualification << endl;
+    cout << "Address: " << address << endl;
+}
 
 int main() 
 {
@@ -35,9 +37,8 @@ int main()
 
     cout << "Enter Faculty ID: ";
     cin >> id;
+    cin.ignore();  
     cout << "Enter Name: ";
-    getline(cin, name);
-    // cin.ignore();
     getline(cin, name);
     cout << "Enter Post: ";
     getline(cin, post);
@@ -48,6 +49,59 @@ int main()
 
     faculty.assignInitialValues(id, name, post, qualification, address);
     faculty.display();
-
     return 0;
 }
+
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+// class Faculty
+// {
+//     string id;
+//     string name;
+//     string post;
+//     string qualification;
+//     string address;
+
+// public:
+//     void assignInitialValues(string facultyID, string facultyName, string facultyPost, string facultyQualification, string facultyAddress) {
+//         id = facultyID;
+//         name = facultyName;
+//         post = facultyPost;
+//         qualification = facultyQualification;
+//         address = facultyAddress;
+//     }
+
+//     void display() {
+//         cout << "Faculty ID: " << id << endl;
+//         cout << "Name: " << name << endl;
+//         cout << "Post: " << post << endl;
+//         cout << "Qualification: " << qualification << endl;
+//         cout << "Address: " << address << endl << endl;
+//     }
+// };
+
+// int main() 
+// {
+//     Faculty faculty;
+//     string id, name, post, qualification, address;
+
+//     cout << "Enter Faculty ID: ";
+//     cin >> id;
+//     cout << "Enter Name: ";
+//     getline(cin, name);
+//     // cin.ignore();
+//     getline(cin, name);
+//     cout << "Enter Post: ";
+//     getline(cin, post);
+//     cout << "Enter Qualification: ";
+//     getline(cin, qualification);
+//     cout << "Enter Address: ";
+//     getline(cin, address);
+
+//     faculty.assignInitialValues(id, name, post, qualification, address);
+//     faculty.display();
+
+//     return 0;
+// }

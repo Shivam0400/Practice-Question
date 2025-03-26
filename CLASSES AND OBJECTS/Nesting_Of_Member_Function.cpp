ae49e55a3652007c6a75ef7c59fed45a0a6dbe3e
Shivam Jaiswal <153131745+Shivam0400@users.noreply.github.com>
2025-03-26 20:04:18 +0530

    Add files via upload

---
+++ CLASSES AND OBJECTS/Nesting_Of_Member_Function.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class binary
+{
+    string s;
+    public:
+    void read(void)
+    {
+        cout<<"Enter a binary number: ";
+        cin>>s;
+    }
+    void chk_bin(void)
+    {
+        for(int i=0;i<s.length();i++)
+        {
+            if (s.at (i) != '0' && s.at(i) != '1')
+            {
+                cout<<"\n Incorrect Binary number format... The program will quit.";
+                exit(0);
+            }
+        }
+    }
+    void ones (void)
+    {
+        chk_bin ();
+        for(int i=0; i < s.length(); i++)
+        {
+            if(s.at (i) == '0')
+            s.at (i) = '1';
+            else
+            s.at (i) = '0';
+        }
+    }
+    void displayones( void )
+    {
+        ones();
+        cout<<"\n The 1's compliment of the above binary number is: "<<s ;
+    }
+};  
+int main()
+{
+    binary b;
+    b.read();
+    b.displayones();
+    return 0;
+}
